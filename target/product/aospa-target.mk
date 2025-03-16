de9d95202ee8cb7dbb83fc4f08e926f82099092c
Rve27 <rve27github@gmail.com>
2025-03-17 02:12:40 +0800

    aospa: Remove Abstruct
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Abstruct
-PRODUCT_PACKAGES += \
-    Abstruct
-
 # AOSPA Version.
 $(call inherit-product, vendor/aospa/target/product/version.mk)
 
